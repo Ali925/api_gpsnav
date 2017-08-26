224214d105df558a4594affb29db57d48b9a5394
Ali925 <alitural.mehdi@gmail.com>
2017-08-27 00:40:13 +0400

    npm srart

--- index.js
+++ index.js
@@ -389,6 +389,6 @@ app.post('/del/sectors', function(req, res){
 	});
 });
 
-app.listen(3000, function(){
-	console.log('Running on 3000!');
+app.listen(443, function(){
+	console.log('Running on 443!');
 });
