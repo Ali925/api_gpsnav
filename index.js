d2d5dee0e1598116405e53fe8457cee2a09a0bcd
Ali925 <alitural.mehdi@gmail.com>
2017-09-28 17:38:55 +0400

    last

--- index.js
+++ index.js
@@ -14,14 +14,7 @@ app.use(session({keys: ['secret']}));
 
 var sql = require('./models/sqlConn');  
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
+app.use(cors());
 app.set('port', (process.env.PORT || 5000));
 
 var usersObj = {};
