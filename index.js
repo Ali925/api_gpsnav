5f71d808c3c2c9caef58ad93f9383d4eb602fd02
Ali925 <alitural.mehdi@gmail.com>
2017-09-28 17:23:50 +0400

    error catch

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use(session({keys: ['secret']}));
 
 var sql = require('./models/sqlConn');  
 
-app.use(cors({
-  "origin": true,
-  "credentials": true,
-  "methods": "OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": true,
-  "optionsSuccessStatus": 204
-}));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('port', (process.env.PORT || 5000));
 
