fa4bfd969ed21703d19a6d5bb0fb5de13d257262
Ali925 <alitural.mehdi@gmail.com>
2017-09-28 17:07:35 +0400

    error catch

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ app.use(session({keys: ['secret']}));
 
 var sql = require('./models/sqlConn');  
 
-app.use(cors());
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": true,
+  "optionsSuccessStatus": 204
+}));
 app.set('port', (process.env.PORT || 5000));
 
 var usersObj = {};
