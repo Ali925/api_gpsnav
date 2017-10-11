258740a160e39e23aa5f20151a213cf0e3fd5b34
Ali925 <alitural.mehdi@gmail.com>
2017-10-11 17:12:24 +0400

    limit

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app  = express();
 var crypto = require('crypto');
 
 var bodyParser = require('body-parser');
-app.use(bodyParser());
 
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
